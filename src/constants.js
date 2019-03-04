28f44d471cd67b575de52e2be1ed5c174dfa116e
synerzip <synerzip@syn-macpro-55.local>
2019-03-04 15:37:31 +0530

    health route added

--- src/constants.js
+++ src/constants.js
@@ -1,11 +1,18 @@
-const PORT = process.env.PORT || 4000; // port to start server on
-const MONGO_PORT = 27017; // mongo port
-const DB_NAME = 'RACE_APP'; // sample db name
-const SECRET = "APISECRET";
+const PORT = process.env.PORT || 3000; // port to start server on
+const MONGO_PORT = process.env.DB_PORT || 27017; // mongo port
+const DB_NAME = process.env.DB_NAME || 'RACE_APP'; // sample db name
+const SECRET = process.env.SECRET || "RMMSECRET";
+const DB_URL = process.env.DB_URL || 'localhost';
+const DB_USERNAME = process.env.DB_USERNAME || '';
+const DB_PASSWORD = process.env.DB_PASSWORD || '';
 
 export {
 	PORT,
 	MONGO_PORT,
 	DB_NAME,
-	SECRET
+	SECRET,
+	DB_URL,
+	DB_USERNAME,
+	DB_PASSWORD,
+
 }
